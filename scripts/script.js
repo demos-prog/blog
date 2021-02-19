2322ab4f7829312f860d8a4d38157f9b2b88327f
Дмитрий <demosne@gmail.com>
2021-02-19 14:27:19 +0300

    aside bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 "use strict"
+
+let btns = document.querySelectorAll('.panel__btn');
+
+
+for (const btn of btns) {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle("selected");
+  })
+}
+
+// let selectedBtn = document.querySelector('.panel__btn, .selected');
+// selectedBtn.classList.remove("selected");
+// let bool = btn.getAttribute("aria-selected");
