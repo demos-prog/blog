54b02cc7ba290639af5ff088499bc3a3da768908
Дмитрий <demosne@gmail.com>
2021-02-22 11:34:18 +0300

    adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,37 @@ for (const btn of btns) {
 // let selectedBtn = document.querySelector('.panel__btn, .selected');
 // selectedBtn.classList.remove("selected");
 // let bool = btn.getAttribute("aria-selected");
+
+// Burger - start
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.secondMenu');
+
+menuIcon.addEventListener('click', ()=>{
+  menuIcon.classList.toggle('active');
+  menu.classList.toggle('hide');
+});
+// Burger - end
+
+// Перестановка в хедере - начало
+let firstMenu = document.querySelector('.headerCont__nav');
+let burger = document.querySelector('.contain');
+
+window.onresize = ()=> {
+  if (window.innerWidth <= 620) {
+    firstMenu.classList.add("hide");
+    burger.classList.remove("hide");
+  } else {
+    firstMenu.classList.remove("hide");
+    burger.classList.add("hide");
+  }
+}
+
+if (window.innerWidth <= 620) {
+  firstMenu.classList.add("hide");
+  burger.classList.remove("hide");
+} else {
+  firstMenu.classList.remove("hide");
+  burger.classList.add("hide");
+}
+
+// Перестановка в хедере - конец
